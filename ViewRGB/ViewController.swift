dfe8e3e419d6a85da0bb9acb3552f4c02a62a6dd
Kirill <133357229+kirillussr@users.noreply.github.com>
2023-06-05 22:29:58 +0300

    Finish home work

--- ViewRGB/ViewController.swift
+++ ViewRGB/ViewController.swift
@@ -8,12 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - IBOulets
+    
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var redRange: UILabel!
+    @IBOutlet var greenRange: UILabel!
+    @IBOutlet var blueRange: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mainView.layer.cornerRadius = 20
+        setupRedSlider()
     }
-
-
+    
+    //MARK: - IBActions
+    
+    @IBAction func redSliderAction() {
+        
+        setupRadRande()
+        
+        mainView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    //MARK: - Private Methods
+    
+    private func setupRadRande(){
+        
+        redRange.text = String(format: "%.2f", redSlider.value)
+        greenRange.text = String(format: "%.2f", greenSlider.value)
+        blueRange.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    private func setupRedSlider() {
+        
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        
+        
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+    }
 }
 
